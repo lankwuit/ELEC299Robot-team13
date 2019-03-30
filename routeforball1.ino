5635bbcd935c85145b2695fb752193814593b3de
KrishnaIyer99 <32946254+KrishnaIyer99@users.noreply.github.com>
2019-03-30 17:13:09 -0400

    Code for the route to get first ball

--- routeforball1.ino
+++ routeforball1.ino
@@ -1,4 +1,20 @@
 void firstball(int speed){
+  int bumpers = 0;
+  do{
     followLine(speed, 10, false);
-  return;
+    bumpers = bumper();
+  }while(bumpers == 0); 
+
+  if(bumpers == 1){
+    //grabs ball 
+  }
+  turnAround(speed);
+  do{
+    followLine(speed, 10, false);
+    bumpers = bumper();
+  }while(bumpers == 0);
+  
+  //release ball
+  turnAround(speed); //come back to original position
+    return;
 }
